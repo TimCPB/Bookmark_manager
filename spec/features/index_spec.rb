054b7902974dd3ee092b709c4ba68b127813d198
Tim <TimCPB@github.com>
2020-06-05 14:52:33 +0100

    more refactoring

--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -5,6 +5,6 @@ require 'sinatra'
 feature 'Bookmark Manager' do
   scenario 'it should produce bookmark homepage' do
     visit '/'
-    expect(page).to have_content 'Welcome to Bookmark Manager!'
+    expect(page).to have_link('View Bookmarks', href: 'http://localhost:9292/bookmarks')
   end
 end
